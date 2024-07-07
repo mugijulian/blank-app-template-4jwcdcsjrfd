1e1e3b76f45b8168ecafc245b9b06048673a053c
mugijulian <julianmsimon04@gmail.com>
2024-07-07 11:20:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,25 @@
+import requests
 import streamlit as st
+
+def fetch_countries():
+    url = "https://api.airvisual.com/v2/countries?key=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"
+    response = requests.get(url)
+    data = response.json()
+    countries = [country['country'] for country in data['data']]
+    return countries
+
+def main():
 
 st.title("Julian's Weather and Air Quality Application")
 st.subheader(
     "Using Streamlit & AirVisual API"
 )
 
+countries = fetch_countries()
+
 option = st.selectbox(
     "Choose a country: ",
-    (url == https://api.airvisual.com/v2/countries?key=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),
+    countries;
     index = None,
     placeholder = "Select country..",
 )
