98c59760d0fefd7c9e06bf9d35796540d0b8ed6e
mugijulian <julianmsimon04@gmail.com>
2024-07-07 10:30:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("Julian's Weather and Air Quality Application")
 st.subheader(
     "Using Streamlit & AirVisual API"
 )
+
+option = st.selectbox(
+    "Select a country please: ",
+    (http://api.airvisual.com/v2/countries?key={{YOUR_API_KEY}}))
