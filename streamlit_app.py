1f5280bffe66742ca700a803bffd7339ffee64f8
mugijulian <julianmsimon04@gmail.com>
2024-07-07 10:24:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Julian's Weather and Air Quality application")
+st.title("Julian's Weather and Air Quality Application")
 st.write(
     "Using Streamlit & AirVisual API"
 )
