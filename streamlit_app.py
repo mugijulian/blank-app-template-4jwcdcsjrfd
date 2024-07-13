4d290fc61e0ba0dcb7d4f0e9b18653db42de2341
mugijulian <julianmsimon04@gmail.com>
2024-07-13 13:15:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-api_key = "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"
+api_key = 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
 
 st.title("Weather and Air Quality Web App")
 st.header("Streamlit and AirVisual API")
