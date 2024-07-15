e1b8037bf7de3eb83721579e80f724c46f85cb32
mugijulian <julianmsimon04@gmail.com>
2024-07-14 22:48:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import requests
-import time
 
-api_key = "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"
+api_key = "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"
 
 st.title("Weather and Air Quality Web App")
 st.header("Streamlit and AirVisual API")
 
-@st.cache_data(show_spinner=False)
+@st.cache_data
 def map_creator(latitude, longitude):
     from streamlit_folium import folium_static
     import folium
@@ -21,40 +20,26 @@ def map_creator(latitude, longitude):
     # call to render Folium map in Streamlit
     folium_static(m)
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data
 def generate_list_of_countries():
     countries_url = f"https://api.airvisual.com/v2/countries?key={api_key}"
-    response = requests.get(countries_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    countries_dict = requests.get(countries_url).json()
+    # st.write(countries_dict)
+    return countries_dict
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data
 def generate_list_of_states(country_selected):
     states_url = f"https://api.airvisual.com/v2/states?country={country_selected}&key={api_key}"
-    response = requests.get(states_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    states_dict = requests.get(states_url).json()
+    # st.write(states_dict)
+    return states_dict
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data
 def generate_list_of_cities(state_selected, country_selected):
     cities_url = f"https://api.airvisual.com/v2/cities?state={state_selected}&country={country_selected}&key={api_key}"
-    response = requests.get(cities_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-def get_aqi_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error("Too many requests. Wait for a few minutes before your next API call.")
-        return None
+    cities_dict = requests.get(cities_url).json()
+    # st.write(cities_dict)
+    return cities_dict
 
 # Include a select box for the options: ["By City, State, and Country","By Nearest City (IP Address)","By Latitude and Longitude"]
 # and save its selected option in a variable called category
@@ -65,30 +50,30 @@ category = st.selectbox(
 
 if category == "By City, State, and Country":
     countries_dict = generate_list_of_countries()
-    if countries_dict and countries_dict["status"] == "success":
+    if countries_dict["status"] == "success":
         countries_list = [i["country"] for i in countries_dict["data"]]
         countries_list.insert(0, "")
 
         country_selected = st.selectbox("Select a country", options=countries_list)
         if country_selected:
             states_dict = generate_list_of_states(country_selected)
-            if states_dict and states_dict["status"] == "success":
+            if states_dict["status"] == "success":
                 states_list = [i["state"] for i in states_dict["data"]]
                 states_list.insert(0, "")
 
                 state_selected = st.selectbox("Select a state", options=states_list)
                 if state_selected:
                     cities_dict = generate_list_of_cities(state_selected, country_selected)
-                    if cities_dict and cities_dict["status"] == "success":
+                    if cities_dict["status"] == "success":
                         cities_list = [i["city"] for i in cities_dict["data"]]
                         cities_list.insert(0, "")
 
                         city_selected = st.selectbox("Select a city", options=cities_list)
                         if city_selected:
                             aqi_data_url = f"https://api.airvisual.com/v2/city?city={city_selected}&state={state_selected}&country={country_selected}&key={api_key}"
-                            aqi_data_dict = get_aqi_data(aqi_data_url)
+                            aqi_data_dict = requests.get(aqi_data_url).json()
 
-                            if aqi_data_dict and aqi_data_dict["status"] == "success":
+                            if aqi_data_dict["status"] == "success":
                                 data = aqi_data_dict["data"]
                                 st.subheader(f"Weather and Air Quality for {city_selected}, {state_selected}, {country_selected}")
                                 st.write(f"Temperature: {data['current']['weather']['tp']} °C")
@@ -109,9 +94,9 @@ if category == "By City, State, and Country":
 
 elif category == "By Nearest City (IP Address)":
     url = f"https://api.airvisual.com/v2/nearest_city?key={api_key}"
-    aqi_data_dict = get_aqi_data(url)
+    aqi_data_dict = requests.get(url).json()
 
-    if aqi_data_dict and aqi_data_dict["status"] == "success":
+    if aqi_data_dict["status"] == "success":
         data = aqi_data_dict["data"]
         st.subheader("Weather and Air Quality for Your Nearest City")
         st.write(f"City: {data['city']}")
@@ -131,9 +116,9 @@ elif category == "By Latitude and Longitude":
 
     if latitude and longitude:
         url = f"https://api.airvisual.com/v2/nearest_city?lat={latitude}&lon={longitude}&key={api_key}"
-        aqi_data_dict = get_aqi_data(url)
+        aqi_data_dict = requests.get(url).json()
 
-        if aqi_data_dict and aqi_data_dict["status"] == "success":
+        if aqi_data_dict["status"] == "success":
             data = aqi_data_dict["data"]
             st.subheader("Weather and Air Quality for Provided Coordinates")
             st.write(f"City: {data['city']}")
